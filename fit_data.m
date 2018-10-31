77becf414dded162ad6f606d44f97d4af99f0f38
keren421 <kerenbenzvi@gmail.com>
2018-10-31 23:52:59 +0200

    changed units + try to isolate only rise

--- fit_data.m
+++ fit_data.m
@@ -1,4 +1,4 @@
-switch 800
+switch 900
     case 700
         A = load('700.mat');
     case 800
@@ -7,18 +7,46 @@ switch 800
         A = load('900.mat');
 end
 
-t_pores = A.C(:,1); %ps
+t_pores = A.C(:,1)*1e-9; %s
 num_pores = A.C(:,2);
-t_pressure = A.P(:,1); %ps
-pressure = A.P(:,2); %Gpa
-Temp = A.P(:,3); %K
+t_pressure = A.P(:,1)*1e-9; %s
+pressure = A.P(:,2)*1e9; %pa
+temp = A.P(:,3); %K
 
 clear A
 %dn/dt = A*exp(-(E-v*P(t))/kT),  needs to find A E and v
+kb = 1.38*1e-23; %m^2*kg*s^-2*K-1
 dndt = gradient(num_pores,t_pores);
 
 figure();
-subplot(2,1,1)
+subplot(3,1,1)
 plot(t_pores, num_pores);
-subplot(2,1,2)
+subplot(3,1,2)
 plot(t_pressure, pressure);
+subplot(3,1,3)
+plot(t_pressure, temp);
+
+[~,i_t] = max(num_pores);
+t_max = t_pores(i_t);
+t_P =t_pressure(t_pressure<t_max);
+P =pressure(t_pressure<t_max);
+Temp = temp(t_pressure<t_max);
+
+figure();
+subplot(3,1,1)
+plot(t_pores(1:i_t), num_pores(1:i_t));
+subplot(3,1,2)
+plot(t_P, P);
+subplot(3,1,3)
+plot(t_P, Temp);
+
+A = 1e25;
+E = 275*kb*700;
+v = 1e-18;
+dn_dt_calc = A*exp(-(E-v*pressure)./(kb*Temp));
+figure();
+subplot(2,1,1)
+plot(t_pressure, dn_dt_calc);
+subplot(2,1,2); hold on;
+plot(t_pressure, cumtrapz(t_pressure,dn_dt_calc));
+plot(t_pores, num_pores);
